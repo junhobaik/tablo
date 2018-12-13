fbb0d6c83454cf0a255fad46e4d342c3e2aa1e9b
Junho Baik <junhobaik@gmail.com>
2018-12-13 18:22:01 +0900

    boilerplate 클론 후 프로젝트에 맞게 수정
    - react 환경으로 수정
    - 프로젝트에 맞게 구조 변경
    
    git clone git@github.com:samuelsimoes/chrome-extension-webpack-boilerplate.git

---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+var webpack = require("webpack"),
+  path = require("path"),
+  fileSystem = require("fs"),
+  env = require("./utils/env"),
+  CleanWebpackPlugin = require("clean-webpack-plugin"),
+  CopyWebpackPlugin = require("copy-webpack-plugin"),
+  HtmlWebpackPlugin = require("html-webpack-plugin"),
+  WriteFilePlugin = require("write-file-webpack-plugin");
+
+var alias = {};
+var secretsPath = path.join(__dirname, "secrets." + env.NODE_ENV + ".js");
+var fileExtensions = [
+  "jpg",
+  "jpeg",
+  "png",
+  "gif",
+  "eot",
+  "otf",
+  "svg",
+  "ttf",
+  "woff",
+  "woff2"
+];
+
+if (fileSystem.existsSync(secretsPath)) {
+  alias["secrets"] = secretsPath;
+}
+
+const options = {
+  entry: {
+    index: "./src/index.js"
+  },
+
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "[name].bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: new RegExp(".(" + fileExtensions.join("|") + ")$"),
+        loader: "file-loader?name=[name].[ext]",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    alias: alias
+  },
+  plugins: [
+    new CleanWebpackPlugin(["build"]),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV)
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: "src/manifest.json",
+        transform: function(content, path) {
+          return Buffer.from(
+            JSON.stringify({
+              description: process.env.npm_package_description,
+              version: process.env.npm_package_version,
+              ...JSON.parse(content.toString())
+            })
+          );
+        }
+      }
+    ]),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+      filename: "index.html",
+      chunks: ["index"]
+    }),
+    new WriteFilePlugin()
+  ]
+};
+
+if (env.NODE_ENV === "development") {
+  options.devtool = "cheap-module-eval-source-map";
+}
+
+module.exports = options;

a703b5c52710e88ffdf2015854236635f4606785
Junho Baik <junhobaik@gmail.com>
2018-12-13 22:25:16 +0900

    마크업 및 스타일 수정

--- src/components/CurrentTabList/index.js
+++ src/components/CurrentTabList/index.js
@@ -7,15 +7,25 @@ class CurrentTabList extends Component {
   render() {
     const tabList = this.props.tabs.map((v, i) => {
       return (
-        <div className="current-tab" key={"tab-" + i}>
-          <img src={v.favIconUrl} alt="" />
+        <li className="tab" key={"tab-" + v.id}>
+          <div className="favicon">
+            {v.favIconUrl ? <img src={v.favIconUrl} alt="favicon" /> : null}
+          </div>
           <a href={v.url}>{v.title}</a>
-          <p>id={v.id}</p>
-        </div>
+        </li>
       );
     });
 
-    return <div>{tabList}</div>;
+    return (
+      <div id="CurrentTabList">
+        <div className="list-wrap">
+          <div className="list-title">
+            <p>[TITLE]</p>
+          </div>
+          <ul className="list">{tabList}</ul>
+        </div>
+      </div>
+    );
   }
 }
 

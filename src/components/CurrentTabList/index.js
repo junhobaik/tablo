2b577c1d12d33840f639c9b718b7924440e36411
Junho Baik <junhobaik@gmail.com>
2018-12-14 20:41:29 +0900

    css를 scss로 변경

--- src/components/CurrentTabList/index.js
+++ src/components/CurrentTabList/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-import "./index.css";
+import "./index.scss";
 
 class CurrentTabList extends Component {
   render() {

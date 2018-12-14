b0189a6cec34bd6ed144fb9c9f0f4765f07856ff
Junho Baik <junhobaik@gmail.com>
2018-12-14 21:04:40 +0900

    App의 기능을 컴포넌트로 이동 및 스타일 변경

--- src/components/App.js
+++ src/components/App.js
@@ -3,45 +3,10 @@ import "./App.scss";
 import CurrentTabList from "./CurrentTabList";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tabs: []
-    };
-  }
-
-  getAllTabs() {
-    chrome.windows.getAll({ populate: true }, windows => {
-      let temp = [];
-      for (let window of windows) {
-        for (let tab of window.tabs) {
-          console.log(tab);
-          const { id, title, url, favIconUrl } = tab;
-          temp.push({ id, title, url, favIconUrl });
-        }
-      }
-      this.setState({
-        tabs: temp
-      });
-    });
-  }
-
-  componentDidMount() {
-    this.getAllTabs();
-
-    chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-      if (changeInfo.status === "complete") this.getAllTabs();
-    });
-
-    chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
-      this.getAllTabs();
-    });
-  }
-
   render() {
     return (
       <div id="App">
-        <CurrentTabList tabs={this.state.tabs} />
+        <CurrentTabList />
       </div>
     );
   }
--- src/components/CurrentTabList/index.js
+++ src/components/CurrentTabList/index.js
@@ -4,14 +4,51 @@ import PropTypes from "prop-types";
 import "./index.scss";
 
 class CurrentTabList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tabs: []
+    };
+  }
+
+  getAllTabs() {
+    chrome.windows.getAll({ populate: true }, windows => {
+      let temp = [];
+      for (let window of windows) {
+        for (let tab of window.tabs) {
+          console.log(tab);
+          const { id, title, url, favIconUrl } = tab;
+          temp.push({ id, title, url, favIconUrl });
+        }
+      }
+      this.setState({
+        tabs: temp
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.getAllTabs();
+
+    chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+      if (changeInfo.status === "complete") this.getAllTabs();
+    });
+
+    chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
+      this.getAllTabs();
+    });
+  }
+
   render() {
-    const tabList = this.props.tabs.map((v, i) => {
+    const tabList = this.state.tabs.map((v, i) => {
       return (
         <li className="tab" key={"tab-" + v.id}>
           <div className="favicon">
             {v.favIconUrl ? <img src={v.favIconUrl} alt="favicon" /> : null}
           </div>
-          <a href={v.url}>{v.title}</a>
+          <a href={v.url} target="_blank">
+            {v.title}
+          </a>
         </li>
       );
     });

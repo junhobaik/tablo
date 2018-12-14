1cb8bba0b11df12ed40a5b12777c1c2022746fab
Junho Baik <junhobaik@gmail.com>
2018-12-14 22:53:13 +0900

    TabList 기본적인 구현 및 redux 적용, 스타일 적용

--- src/components/TabList/index.js
+++ src/components/TabList/index.js
@@ -1,13 +1,44 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-
+import { connect } from "react-redux";
+import { addColumn } from "../../redux/actions";
 import "./index.scss";
 
 class TabList extends Component {
   render() {
+    const tabListObj = this.props.tabList.map((v, i) => {
+      const tabsObj = v.tabs.map((v, i) => {
+        return (
+          <li className="tab" key={"tab-" + v.id}>
+            <div className="favicon">
+              {v.favIconUrl ? <img src={v.favIconUrl} alt="favicon" /> : null}
+            </div>
+            <a href={v.url} target="_blank">
+              {v.title}
+            </a>
+          </li>
+        );
+      });
+      return (
+        <div className="tab-list" key={`tab-list-${v.num}`}>
+          <div className="top">
+            <h2>{v.title}</h2>
+            <div>*</div>
+          </div>
+          <ul>{tabsObj}</ul>
+        </div>
+      );
+    });
+
     return (
       <div id="TabList">
-        <div className="add-column">
+        {tabListObj}
+        <div
+          className="add-column"
+          onClick={() => {
+            this.props.onAddColumn();
+          }}
+        >
           <span>+ Add column</span>
         </div>
       </div>
@@ -15,6 +46,23 @@ class TabList extends Component {
   }
 }
 
+let mapDispatchToProps = dispatch => {
+  return {
+    onAddColumn: () => dispatch(addColumn())
+  };
+};
+
+let mapStateToProps = state => {
+  return {
+    tabList: state.tab.tabList
+  };
+};
+
 TabList.propTypes = {};
+
+TabList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TabList);
 
 export default TabList;

1096b9a441fba10d602158f4c18476d57a6661f1
Junho Baik <junhobaik@gmail.com>
2018-12-14 20:30:44 +0900

    더미 데이터 파일 추가

---
+++ src/dummyData.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    title: "dummy 1",
+    url: "#",
+    favIconUrl: "#"
+  },
+  {
+    id: 2,
+    title: "dummy 2",
+    url: "#",
+    favIconUrl: "#"
+  },
+  {
+    id: 3,
+    title: "dummy 3",
+    url: "#",
+    favIconUrl: "#"
+  },
+  {
+    id: 4,
+    title: "dummy 4",
+    url: "#",
+    favIconUrl: "#"
+  },
+  {
+    id: 5,
+    title: "dummy 5",
+    url: "#",
+    favIconUrl: "#"
+  }
+];

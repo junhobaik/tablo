050afd59e91ab319edcb6f210eaf82ef51498958
Junho Baik <junhobaik@gmail.com>
2018-12-28 21:54:36 +0900

    fontawesome 아이콘 추가

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,9 @@ import CurrentTabList from "./CurrentTabList";
 import TabList from "./TabList";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEllipsisH, faEllipsisV } from '@fortawesome/free-solid-svg-icons'
+import { faEllipsisH, faEllipsisV, faTrashAlt, faEdit } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faEllipsisH, faEllipsisV)
+library.add(faEllipsisH, faEllipsisV, faTrashAlt, faEdit)
 
 class App extends Component {
   render() {

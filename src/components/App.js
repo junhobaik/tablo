f6a4e95d4d855076785eebc31a6aa66115698db5
Junho Baik <junhobaik@gmail.com>
2018-12-31 23:21:39 +0900

    불불필요한 코드 제거

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import "./App.scss";
 import CurrentTabList from "./CurrentTabList";
 import TabList from "./TabList";
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEllipsisH, faEllipsisV, faTrashAlt, faEdit } from '@fortawesome/free-solid-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faEllipsisH,
+  faEllipsisV,
+  faTrashAlt,
+  faEdit
+} from "@fortawesome/free-solid-svg-icons";
+library.add(faEllipsisH, faEllipsisV, faTrashAlt, faEdit);
 
-library.add(faEllipsisH, faEllipsisV, faTrashAlt, faEdit)
+import { loadTabs } from "../redux/actions";
 
 class App extends Component {
+  componentDidMount() {
+    const load = this.props.onLoadTabs;
+    chrome.storage.sync.get("tablo_tab", function(items) {
+      load(items.tablo_tab);
+    });
+  }
+
   render() {
     return (
       <div id="App">
@@ -18,5 +31,21 @@ class App extends Component {
     );
   }
 }
+let mapDispatchToProps = dispatch => {
+  return {
+    onLoadTabs: state => dispatch(loadTabs(state))
+  };
+};
+
+let mapStateToProps = state => {
+  return {};
+};
+
+App.propTypes = {};
+
+App = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
 
 export default App;

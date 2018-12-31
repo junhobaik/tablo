93e72697c163ae0b9b72bfc29e436abf22442876
Junho Baik <junhobaik@gmail.com>
2018-12-31 22:30:56 +0900

    redux 구조 개선

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import Reducer from "./redux/reducers";
+import createReducer from "./redux/reducers";
 
-const store = createStore(Reducer);
+const store = createStore(createReducer());
 
 store.subscribe(() => {
-  localStorage.setItem('tablo', JSON.stringify(store.getState().tab));
+  console.log(store.getState())
+  localStorage.setItem("tablo", JSON.stringify(store.getState().tab));
+
+  chrome.storage.sync.set(
+    {
+      tablo_tab: store.getState().tab
+    },
+    () => {
+      console.log("Save to storage, ", store.getState().tab);
+    }
+  );
 });
 
 ReactDOM.render(
---
+++ src/redux/reducers/app.js
@@ -0,0 +1,12 @@
+const appInitialState = {
+  load: false
+};
+
+const app = (state = appInitialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default app;

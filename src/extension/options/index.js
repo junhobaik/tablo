42f89367d76f4faaf8cfc25abbdde83ecb4c592d
Junho Baik <junhobaik@gmail.com>
2019-01-07 01:19:19 +0900

    options, link open 값 불러오기 구현

--- src/extension/options/index.js
+++ src/extension/options/index.js
@@ -10,4 +10,24 @@ document.addEventListener('DOMContentLoaded', () => {
       document.querySelector('#backupCode').value = code;
     });
   });
+
+  chrome.storage.sync.get('tablo_app', i => {
+    console.log(i.tablo_app);
+    if (i.tablo_app) {
+      if (i.tablo_app.aTarget === '_blank') {
+        document.querySelector('#openBlank').checked = 'true';
+      } else {
+        document.querySelector('#openSelf').checked = 'true';
+      }
+    } else {
+      chrome.storage.sync.set(
+        {
+          tablo_app: {
+            aTarget: '_blank',
+          },
+        },
+        () => {}
+      );
+    }
+  });
 });

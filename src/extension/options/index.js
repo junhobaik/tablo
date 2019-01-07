ac707b5b0975fd85aef32c994af7bd14c25e0a52
Junho Baik <junhobaik@gmail.com>
2019-01-07 23:50:29 +0900

    options, 코드 개선 및 구현

--- src/extension/options/index.js
+++ src/extension/options/index.js
@@ -1,33 +1,56 @@
-document.addEventListener('DOMContentLoaded', () => {
-  chrome.storage.sync.get('tablo_cart', function(items) {
-    const cart = items.tablo_cart;
-
-    chrome.storage.sync.get('tablo_tab', function(items) {
-      const tab = items.tablo_tab;
-
-      const code = JSON.stringify({ tab, cart });
-
-      document.querySelector('#backupCode').value = code;
+const getStorage = (key = null) => {
+  return new Promise(resolve => {
+    chrome.storage.sync.get(key, item => {
+      key ? resolve(item[key]) : resolve(item);
     });
   });
+};
 
-  chrome.storage.sync.get('tablo_app', i => {
-    console.log(i.tablo_app);
-    if (i.tablo_app) {
-      if (i.tablo_app.aTarget === '_blank') {
-        document.querySelector('#openBlank').checked = 'true';
-      } else {
-        document.querySelector('#openSelf').checked = 'true';
-      }
-    } else {
-      chrome.storage.sync.set(
-        {
-          tablo_app: {
-            aTarget: '_blank',
-          },
-        },
-        () => {}
-      );
-    }
+const setStorage = (key, data) => {
+  return new Promise(resolve => {
+    chrome.storage.sync.set(
+      {
+        [key]: data,
+      },
+      () => resolve()
+    );
   });
+};
+
+const renderDataManagement = (tablo_tab, tablo_cart) => {
+  const code = JSON.stringify({ tablo_tab, tablo_cart });
+  document.querySelector('#backupCode').value = code;
+};
+
+const renderBasicSetting = (tablo_app = null) => {
+  if (tablo_app) {
+    if (tablo_app.openLink.link === '_blank') {
+      document.querySelector('#openLinkBlank').checked = 'true';
+    } else {
+      document.querySelector('#openLinkSelf').checked = 'true';
+    }
+    if (tablo_app.openLink.tab === '_blank') {
+      document.querySelector('#openTabBlank').checked = 'true';
+    } else {
+      document.querySelector('#openTabSelf').checked = 'true';
+    }
+  } else {
+    setStorage('tablo_app', {
+      openLink: {
+        link: '_blank',
+        tab: '_blank',
+      },
+    });
+    document.querySelector('#openLinkBlank').checked = 'true';
+    document.querySelector('#openTabBlank').checked = 'true';
+  }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  (async () => {
+    const { tablo_cart, tablo_tab, tablo_app } = await getStorage();
+
+    renderDataManagement(tablo_tab, tablo_cart);
+    renderBasicSetting(tablo_app);
+  })();
 });

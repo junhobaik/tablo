b7b579efb7fd9c3de6db3efcfff125a095511495
Junho Baik <junhobaik@gmail.com>
2019-02-26 00:15:16 +0900

    index.js 코드 개선

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import createReducer from './redux/reducers';
+import App from './components/App';
 
 const store = createStore(createReducer());
 
 store.subscribe(() => {
   localStorage.setItem('tablo_tab', JSON.stringify(store.getState().tab));
 
+  // eslint-disable-next-line no-undef
   chrome.storage.sync.set(
     {
       tablo_tab: store.getState().tab,
